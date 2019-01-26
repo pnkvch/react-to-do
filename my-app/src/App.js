d5641332aeb811cd133851b30672081303712afc
Artur <apinkevich@yahoo.com>
2019-01-26 11:38:23 +0200

    initial

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <ul>
+        <li>Item</li>
+        <li>Item</li>
+        <li>Item</li>
+      </ul>
+    );
+  }
+}
+
+export default App;
