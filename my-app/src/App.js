25be8a0e841073f520a9ee2a2a6e2fb503d5c9b1
Artur <apinkevich@yahoo.com>
2019-02-16 11:07:27 +0200

    mapping components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,12 @@ import React from 'react';
 // import NavBar from "./NavBar"
 import MainContent from "./MainContent"
 // import Footer from "./Footer"
+import data from "./JSON.js"
 
 import './App.css';
 
-const data = fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(response => response.json()).then(json => console.log(json));
-
-const posts = data.map(post => <MainContent key={post.id} punchline={post.title} question={post.body} />)
+const posts = 
+    data.map(post => <MainContent key={post.id} punchline={post.title} question={post.body} />);
 
 function App() {
   return (
