1164ab90a5482212b6d4fa73b2d2d19e643484ba
Artur <apinkevich@yahoo.com>
2019-02-18 15:26:04 +0200

    react practice

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 // import NavBar from "./NavBar"
 import MainContent from "./MainContent"
@@ -10,12 +10,14 @@ import './App.css';
 const posts = 
     data.map(post => <MainContent key={post.id} punchline={post.title} question={post.body} />);
 
-function App() {
-  return (
-    <div>
-      {posts}
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {posts}
+      </div>
+    )
+  }
 };
 
 
