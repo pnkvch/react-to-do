db918425d306d1c58b3325e8ec4daa0d720de8e4
Artur <apinkevich@yahoo.com>
2019-03-02 16:18:35 +0200

    cleaned to-do app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,41 @@
 import React, {Component} from 'react';
-
-// import NavBar from "./NavBar"
-import MainContent from "./MainContent"
-// import Footer from "./Footer"
+import MainContent from './MainContent'
 import data from "./JSON.js"
 
 import './App.css';
 
-const posts = 
-    data.map(post => <MainContent key={post.id} punchline={post.title} question={post.body} />);
-
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todosData: data,
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+      this.setState(prevState => {
+        const updatedTodos = prevState.todosData.map((todo) => {
+          if (todo.id === id) {
+            todo.completed = !todo.completed
+          }
+          return todo
+        })
+        return {
+          updatedTodos
+        }
+      })
+  }
+
   render() {
+    const todos = this.state.todosData.map(item => <MainContent key={item.id} item={item} handleChange={this.handleChange} />)
+
     return (
-      <div>
-        {posts}
+      <div className="todo-list">
+        {todos}
       </div>
     )
   }
-};
-
+}
 
 export default App;
--- my-app/src/MainContent.js
+++ my-app/src/MainContent.js
@@ -2,11 +2,13 @@ import React from "react"
 
 function MainContent(props) {
     return(
-        <div>
-            <input type="checkbox" />
+        <div className="todo-item">
+            <input type="checkbox" 
+            checked={props.item.completed}
+            onChange={() => props.handleChange(props.item.id)}
+            />
             <label>
-                {props.punchline}
-                {props.question}
+                {props.item.text}
             </label>
         </div>
     )
